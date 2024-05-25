41c2b815b3f92f5a88e0a8256d9e574fdb7281dc
Peter Bendel <peter@familie-bendel.de>
2024-05-25 20:32:51 +0200

    first implementation of charge brick

--- charger/vwweconnect.go
+++ charger/vwweconnect.go
@@ -16,33 +16,99 @@ package charger
 // SOFTWARE.
 
 import (
-	"errors"
-	"time"
+	"fmt"
 
 	"github.com/evcc-io/evcc/api"
 	"github.com/evcc-io/evcc/util"
+	"github.com/evcc-io/evcc/util/config"
 )
+
+// WeConnect charger implementation using "Ladeziegel" (charging brick) from Volkswagen and controlled
+// by the WeConnect cloud.
+type WeConnect struct {
+	vehicle api.Vehicle
+	log     *util.Logger
+}
 
 func init() {
 	registry.Add("vwweconnect", NewVWWeConnectFromConfig)
 }
 
+// NewWeConnectFromConfig creates a WeConnect charger from generic config
 func NewVWWeConnectFromConfig(other map[string]interface{}) (api.Charger, error) {
-	cc := struct {
-		Token string
-		URI   string
-		Cache time.Duration
-	}{
-		Cache: time.Second,
+	var cc struct {
+		Vehiclename string
 	}
+
+	log := util.NewLogger("vwweconnect")
 
 	if err := util.DecodeOther(other, &cc); err != nil {
 		return nil, err
 	}
 
-	if (cc.URI != "") == (cc.Token != "") {
-		return nil, errors.New("must have either uri or token")
+	if cc.Vehiclename == "" {
+		return nil, api.ErrMissingCredentials
 	}
 
-	return NewGoE(cc.URI, cc.Token, cc.Cache)
+	veh, err := config.Vehicles().ByName(cc.Vehiclename)
+	if err != nil {
+		return nil, err
+	}
+	vehicle := veh.Instance()
+
+	return NewVWConnect(log, vehicle)
+}
+
+// NewWeConnect creates a new connection with standbypower for charger
+func NewVWConnect(log *util.Logger, vehicle api.Vehicle) (*WeConnect, error) {
+
+	c := &WeConnect{
+		vehicle: vehicle,
+		log:     log,
+	}
+
+	return c, nil
+}
+
+func (wb *WeConnect) MaxCurrent(current int64) error {
+	return fmt.Errorf("MaxCurrent() not implemented for VWWConnect charge brick")
+}
+
+// Status implements the api.Charger interface
+func (c *WeConnect) Status() (api.ChargeStatus, error) {
+	if stateinterface, ok := c.vehicle.(api.ChargeState); ok {
+		return stateinterface.Status()
+	} else {
+		return api.StatusNone, nil
+	}
+}
+
+// Enabled implements the api.Charger interface
+func (c *WeConnect) Enabled() (bool, error) {
+	if stateinterface, ok := c.vehicle.(api.ChargeState); ok {
+		state, err := stateinterface.Status()
+		if err != nil {
+			return false, err
+		}
+		if state == api.StatusC {
+			return true, nil
+		}
+		return false, nil
+	} else {
+		return false, fmt.Errorf("vehicle does not support charge state")
+	}
+}
+
+// Enable implements the api.Charger interface
+func (c *WeConnect) Enable(enable bool) error {
+	if chargerIF, ok := c.vehicle.(api.ChargeController); ok {
+		return chargerIF.ChargeEnable(enable)
+	} else {
+		return fmt.Errorf("vechicle does not support charge controller")
+	}
+}
+
+// TotalEnergy implements the api.MeterEnergy interface
+func (c *WeConnect) TotalEnergy() (float64, error) {
+	return 0.0, fmt.Errorf("vwweconnect charger does not support TotalEnergy")
 }
